4b629a49b3ae2a5979521f49ed8c28ff34b7fd61
BrunoValan <110431113+BrunoValan@users.noreply.github.com>
2023-06-18 01:46:43 +0000

    'deleted requirements versions'

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from app import hello
 
-#pip freeze shows all versions that are isntalled
+
+# pip freeze shows all versions that are isntalled
 def test_hello():
     assert "demo hello" == hello()
